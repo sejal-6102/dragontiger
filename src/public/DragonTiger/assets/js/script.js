5e6bc257c5f9895e87fb8ce3a658172e7d5116a1
Infayou Digital <infayoudigital@Infayous-MacBook-Pro.local>
2025-04-25 12:54:30 +0530

    Initial pushddg

--- src/public/DragonTiger/assets/js/script.js
+++ src/public/DragonTiger/assets/js/script.js
@@ -332,7 +332,9 @@ function initGame(startingCoins, userPhoneNoParam) {
     const values = [ { name: "2", value: 2 }, { name: "3", value: 3 }, { name: "4", value: 4 }, { name: "5", value: 5 }, { name: "6", value: 6 }, { name: "7", value: 7 }, { name: "8", value: 8 }, { name: "9", value: 9 }, { name: "10", value: 10 }, { name: "J", value: 11 }, { name: "Q", value: 12 }, { name: "K", value: 13 }, { name: "A", value: 14 }, ];
     const fullDeck = [];
     suits.forEach((suit) => { values.forEach((val) => { fullDeck.push({ ...val, suit }); }); });
-    const socket = io("http://localhost:3000");
+    // const socket = io("http://localhost:3000");
+    const socket = io("https://bluedoller.online");
+
     let stagedBets = { Dragon: 0, Tie: 0, Tiger: 0 };
     let confirmedBets = {};
     let selectedBetTarget = null;
